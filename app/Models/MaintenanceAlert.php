439d55ef8c9a7d3f7465cbdaa0d3c85aa7d6fbe3
Daniel Ramirez <dani.rg15@gmail.com>
2019-11-25 18:48:44 +0100

    fix

--- app/Models/MaintenanceAlert.php
+++ app/Models/MaintenanceAlert.php
@@ -7,6 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class MaintenanceAlert extends Model
 {
+   
+    protected $fillable = [
+        'vehicle_id',
+        'description'
+    ];
+
     public function vehicle()
     {
         return $this->belongsTo(Vehicle::class);
