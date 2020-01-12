a414dd1131fe551ee7fb5e9a6388dfa73f5a9b2e
Daniel Ramirez <dani.rg15@gmail.com>
2020-01-12 15:29:51 +0100

    Remove ionicos dependency

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -3,7 +3,6 @@
 <head>
 	<title></title>
 	<link href="{{ mix('css/app.css') }}" rel="stylesheet">
-	<script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 </head>
 <body class="bg-gray-200 text-gray-800">
