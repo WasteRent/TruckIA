2e507094749b0b072fa72eaff791904dfe031c88
Daniel Ramirez <dani.rg15@gmail.com>
2020-02-10 22:15:18 +0100

    change progress bar

--- resources/views/garage/repair_orders/execute.blade.php
+++ resources/views/garage/repair_orders/execute.blade.php
@@ -26,7 +26,7 @@
 		</div>
 		<div class="w-full">
 			<div class="w-full">
-			  <div class="shadow-lg w-full bg-gray-300 rounded-full">
+			  <div class="shadow-lg w-full bg-white rounded-full">
 			    <div class="bg-blue-800 text-xs leading-none py-1 text-center text-white rounded-full" style="width: {{ $repair_order->completePercent }}%">{{ $repair_order->completePercent }} %</div>
 			  </div>
 			</div>
