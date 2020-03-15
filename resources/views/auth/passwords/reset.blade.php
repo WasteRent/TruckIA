3d88c8fc78f2972b089480edcf33f09dc7057b30
laura <laura.gago@endado.com>
2020-03-15 17:37:17 +0100

    style form to reset password

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,65 +1,56 @@
 @extends('layouts.auth')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+<div class=" max-w-md mx-auto">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('password.update') }}">
-                        @csrf
 
-                        <input type="hidden" name="token" value="{{ $token }}">
+    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 mt-16" method="POST" action="{{ route('password.update') }}">
+        @csrf
+        <div class="max-w-xs mx-auto">
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+            <img class="mx-auto h-12 w-auto" src="https://truckts.com/img/logos/truckts_logo.png" alt="truckts-logo" />
+            <h2 class="mt-6 mb-3 text-center text-xl leading-9 font-bold text-gray-800 ">
+                {{ __('Cambiar contraseña') }}</h2>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Reset Password') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+            <div>
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="email
+                "> Email</label>
+                <input placeholder="Email" id="email" type="email" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5 @error('email') border-red-500 @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
             </div>
+            <div>
+                <label class="block text-gray-700 text-sm font-bold mb-2 mt-3" for="password"> Escriba una contraseña</label>
+                <input placeholder="Nueva contraseña" id="password" type="password" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5 @error('password') border-red-500 @enderror" name="password" required autocomplete="new-password">
+            </div>
+            <div>
+                <label class="block text-gray-700 text-sm font-bold mb-2 mt-3" for="password-confirm"> Repita la contraseña</label>
+                <input placeholder="Repita la contraseña" id="password-confirm" type="password" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5 @error('password') border-red-500 @enderror" name="password_confirmation" />
+            </div>
+
+            @error('email')
+            <p class="text-red-500 text-xs italic mt-1" role="alert">
+                <strong>{{ $message }}</strong>
+            </p>
+            @enderror
+            @error('password')
+            <p class="text-red-500 text-xs italic mt-1" role="alert">
+                <strong>{{ $message }}</strong>
+            </p>
+            @enderror
+            <div class="mt-6">
+                <button type="submit" class="group w-full flex justify-center py-2 px-4 border border-transparent text-sm font-bold rounded-md text-white bg-green-truckts hover:bg-green-trucktslighter focus:outline-none focus:border-green-700 focus:shadow-outline-green active:bg-green-truckts transition duration-150 ease-in-out">
+                    {{ __('Cambiar contraseña') }}
+                </button>
+            </div>
+
         </div>
+
+    </form>
+    <div>
+        <p class="text-center text-gray-500 text-xs">
+            &copy;2020 TruckTs. All rights reserved.
+        </p>
     </div>
 </div>
+
+
 @endsection
