59676272d662a458f152b4d878de588a7774b562
Daniel Ramirez <dani.rg15@gmail.com>
2020-03-19 21:56:07 +0100

    add alerts table

---
+++ database/migrations/2020_03_19_214836_create_alerts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAlertsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('alerts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('vehicle_id');
+            $table->string('title');
+            $table->text('description');
+            $table->boolean('dismissed')->default(false);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('vehicle_id')->references('id')->on('vehicles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('alerts');
+    }
+}
