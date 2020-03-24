c6891a7641267675499bff402509ef260bfce66e
Daniel Ramirez <dani.rg15@gmail.com>
2020-03-24 13:50:27 +0100

    add help

--- app/Http/Controllers/Customer/CustomerDetailsController.php
+++ app/Http/Controllers/Customer/CustomerDetailsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Customer;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\CustomerRequest;
+use App\Models\EnterpriseGroup;
 use Illuminate\Support\Facades\Auth;
 
 class CustomerDetailsController extends Controller
@@ -12,7 +13,8 @@ class CustomerDetailsController extends Controller
     public function index()
     {
         return view('customer.details', [
-            'customer' => Auth::user()->customer
+            'customer' => Auth::user()->customer,
+            'enterprises' => EnterpriseGroup::all()
         ]);
     }
 
--- resources/views/customer/failures/create.blade.php
+++ resources/views/customer/failures/create.blade.php
@@ -13,6 +13,11 @@
 
 		@include('customer.failures.form')
 
+		<a class="text-indigo-600" target="_blank" href="https://www.allianz.es/centro-respuesta-rapida/peticion-grua">
+			<i class="fas fa-life-ring"></i>
+			Necesito grua
+		</a>
+		
 		<div class="flex justify-end">
 			<button class="px-4 py-1 rounded text-white bg-indigo-600 shadow flex items-center">Guardar</button>
 		</div>
