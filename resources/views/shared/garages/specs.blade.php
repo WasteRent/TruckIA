922a2b16108fd236c7e2933ba8562e1f9b00e9a2
Daniel Ramirez <dani.rg15@gmail.com>
2020-03-29 17:55:57 +0200

    group specs

--- resources/views/shared/garages/specs.blade.php
+++ resources/views/shared/garages/specs.blade.php
@@ -1,11 +1,16 @@
 <div>
 	@if(isset($garage))
-		@foreach($garage->specialities as $spec)
-			@if($spec->pivot->stars > 0)
-			<div class="flex items-center">
-				<span class="text-xs w-16 mr-2">{{ $spec->name }}</span>
-				<stars :rating="{{ $spec->pivot->stars }}"></stars>
-			</div>
+		@foreach($garage->specialities->groupBy('group') as $specs)
+			@foreach($specs as $spec)
+				@if($spec->pivot->stars > 0)
+				<div class="flex items-center">
+					<span class="text-xs w-16 mr-2">{{ $spec->name }}</span>
+					<stars :rating="{{ $spec->pivot->stars }}"></stars>
+				</div>
+				@endif
+			@endforeach
+			@if(!$loop->last)
+				<hr class="my-2">
 			@endif
 		@endforeach
 	@endif
