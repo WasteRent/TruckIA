758039c2ac6e17c2a2063c6f61d1f12a5a3c65e0
Daniel Ramirez <dani.rg15@gmail.com>
2020-04-05 21:21:24 +0200

    add deploy script

---
+++ pre-deploy.sh
@@ -0,0 +1,10 @@
+git checkout dev
+git pull
+yarn install
+npm run dev
+composer install --no-dev && composer update --no-dev
+php artisan migrate --force
+php artisan route:cache
+php artisan config:cache
+php artisan view:cache
+composer dump --optimize
