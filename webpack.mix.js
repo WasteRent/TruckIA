b9f2ecc14348585a55fa3883bb3d96aadb603638
Daniel Ramirez <dani.rg15@gmail.com>
2020-04-05 21:30:00 +0200

    add class whitelist

--- webpack.mix.js
+++ webpack.mix.js
@@ -34,7 +34,22 @@ if (mix.inProduction()) {
         whitelist: [
           'pagination',
           'page-item',
-          'page-link'
+          'page-link',
+          'bg-yellow-100',
+          'text-yellow-800',
+          'border-yellow-200',
+          'bg-green-100',
+          'text-green-800',
+          'border-green-200',
+          'bg-blue-100',
+          'text-blue-800',
+          'border-blue-200',
+          'bg-gray-100',
+          'text-gray-800',
+          'border-gray-200',
+          'bg-red-100',
+          'text-red-800',
+          'border-red-200'
         ],
         extractors: [
           {
