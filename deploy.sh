08f561cbaaca66f6a3e1dea502489b6f75c33b28
Daniel Ramirez <dani.rg15@gmail.com>
2020-04-08 17:19:06 +0200

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
+php artisan cache:clear
 yarn install
 npm run prod
-composer install --no-dev && composer update --no-dev
+composer update --no-dev
 php artisan migrate --force
 php artisan route:cache
 php artisan config:cache
