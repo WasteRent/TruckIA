08a897a5c2a9afdabb3fa6205a65395decba2233
Daniel Ramirez <dani.rg15@gmail.com>
2020-04-15 21:31:02 +0200

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
-php artisan view:cache
+#php artisan view:cache
 yarn install
 npm run prod
 composer update --no-dev --optimize-autoloader
 php artisan migrate --force
-php artisan route:cache
-php artisan config:cache
+#php artisan route:cache
+#php artisan config:cache
