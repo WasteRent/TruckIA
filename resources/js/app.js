bede31f812020a39d1155cbc0b89d79d0bb151a1
Daniel Ramirez <dani.rg15@gmail.com>
2020-04-23 09:54:26 +0200

    install turbolinks

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,9 @@ require('./bootstrap');
 window.$ = require('jquery');
 
 
+var Turbolinks = require("turbolinks")
+Turbolinks.start()
+
 window.ajaxSelect = function(trigger, target, source) {
     $(`select[name="${target}"]`).find('option').remove();
 
