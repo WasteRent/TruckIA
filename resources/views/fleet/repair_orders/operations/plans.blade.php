957ba0182c9e36d7a1e3ee85eaad2290f365bc15
Daniel Ramirez <dani.rg15@gmail.com>
2020-06-29 22:49:21 +0200

    add counters in plans

--- resources/views/fleet/repair_orders/operations/plans.blade.php
+++ resources/views/fleet/repair_orders/operations/plans.blade.php
@@ -39,18 +39,32 @@
 		  	  <td>{{ $plan->name }}</td>
 		  	  <td>{{ optional($plan->manufacturer)->name }} {{ optional($plan->model)->name }}</td>
 		  	  <td>
-		  	  	@if($plan->kms)
-		  	  		{{ $plan->kms }} kms <br>
-		  	  	@endif
-		  	  	@if($plan->natural_hours)
-		  	  		{{ $plan->natural_hours }} Horas Naturales <br>
-		  	  	@endif
-		  	  	@if($plan->work_hours)
-		  	  		{{ $plan->work_hours }} Horas de Trabajo <br>
-		  	  	@endif
-		  	  	@if($plan->can_hours)
-		  	  		{{ $plan->can_hours }} Horas CAN <br>
-		  	  	@endif
+		  	  	<div class="">
+		  	  		@if($plan->kms)
+		  	  			<div class="flex">
+			  	  			<div class="w-3/4">{{ $plan->kms }} kms</div>
+			  	  			<div class="w-1/4">
+			  	  				@include('fleet.vehicles.counters.progress-slim', ['counter' => $repair_order->vehicle->counters->where('type', 'work_hours')->where('max', $plan->can_hours)->first()])
+			  	  			</div>
+		  	  			</div>
+		  	  		@endif	
+		  	  		@if($plan->natural_hours)
+		  	  			<div class="flex">
+		  	  				<div class="w-3/4">{{ $plan->natural_hours }} Horas Naturales</div>
+		  	  				<div class="w-1/4">
+		  	  					@include('fleet.vehicles.counters.progress-slim', ['counter' => $repair_order->vehicle->counters->where('type', 'natural_hours')->where('max', $plan->natural_hours)->first()])
+		  	  				</div>
+		  	  			</div>
+		  	  		@endif
+		  	  		@if($plan->can_hours)
+		  	  			<div class="flex">
+		  	  				<div class="w-3/4">{{ $plan->can_hours }} Horas CAN</div>
+		  	  				<div class="w-1/4">
+		  	  					@include('fleet.vehicles.counters.progress-slim', ['counter' => $repair_order->vehicle->counters->where('type', 'work_hours')->where('max', $plan->can_hours)->first()])
+		  	  				</div>
+		  	  			</div>
+		  	  		@endif
+		  	  	</div>
 		  	  </td>
 		  	  <td>
 		  	  	<form method="POST" action="{{ route('fleet.repair-orders.maintenance-plans.store', $repair_order) }}">
---
+++ resources/views/fleet/vehicles/counters/progress-slim.blade.php
@@ -0,0 +1,27 @@
+@php
+	$percent = ($counter->current / $counter->max) * 100;
+	$color = 'progress-bar-step1';
+	if($percent > 70 && $percent < 90) {
+		$color = 'progress-bar-step2';
+	} elseif($percent > 90 && $percent < 100) {
+		$color = 'progress-bar-step3';
+	} elseif($percent > 100) {
+		$color = 'bg-red-500';
+	} 
+@endphp
+
+<div class="text-xs">
+	{{number_format($counter->current)}}
+	@if($counter->type == 'natural_hours')
+		H
+	@elseif($counter->type == 'work_hours')
+		H
+	@elseif($counter->type == 'kms')
+		kms
+	@endif
+</div>
+
+<div class="rounded-full">
+	<div role="progressbar" aria-valuenow="{{number_format($counter->current)}}" aria-valuemin="0" aria-valuemax="{{$counter->max}}" class="{{$color}} h-1 text-xs leading-none text-center text-white rounded-full" style="width: {{ $percent > 100 ? 100 : $percent }}%">
+	</div>
+</div>
