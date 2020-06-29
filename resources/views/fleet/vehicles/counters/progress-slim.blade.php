696185f55e2adf90e2df41923c9f0b675fdcee7d
Daniel Ramirez <dani.rg15@gmail.com>
2020-06-29 23:09:37 +0200

    fix

--- resources/views/fleet/vehicles/counters/progress-slim.blade.php
+++ resources/views/fleet/vehicles/counters/progress-slim.blade.php
@@ -1,3 +1,5 @@
+@if($counter)
+
 @php
 	$percent = ($counter->current / $counter->max) * 100;
 	$color = 'progress-bar-step1';
@@ -25,3 +27,5 @@
 	<div role="progressbar" aria-valuenow="{{number_format($counter->current)}}" aria-valuemin="0" aria-valuemax="{{$counter->max}}" class="{{$color}} h-1 text-xs leading-none text-center text-white rounded-full" style="width: {{ $percent > 100 ? 100 : $percent }}%">
 	</div>
 </div>
+
+@endif
