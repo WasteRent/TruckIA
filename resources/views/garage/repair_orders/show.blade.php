e50fd7560231ac7a318c7486abfc0455003bb6e7
Daniel Ramirez <dani.rg15@gmail.com>
2020-06-30 10:11:42 +0200

    print operations in garage summary view

--- resources/views/garage/repair_orders/show.blade.php
+++ resources/views/garage/repair_orders/show.blade.php
@@ -43,6 +43,12 @@
 	@component('components.card', ['is_table' => true])
 		@slot('title', 'Operaciones')
 
+		@slot('corner')
+			<a class="mr-6" href="{{ route('garage.repair-orders.operations.pdf', $repair_order) }}" target="_blank">
+				<i class="fas fa-file-pdf fa-lg text-red-700"></i> Imprimir
+			</a>
+		@endslot
+
 		<table>
 		  <thead>
 		    <tr>
