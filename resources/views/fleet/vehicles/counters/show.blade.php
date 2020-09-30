ab2e0c9dfed9f6354d2d1231cac3ec39fe747b96
Daniel Ramirez <dani.rg15@gmail.com>
2020-09-30 17:17:40 +0200

    separate counter of chassis and equipment in vehicle card

--- resources/views/fleet/vehicles/counters/show.blade.php
+++ resources/views/fleet/vehicles/counters/show.blade.php
@@ -1,21 +1,39 @@
 @if($vehicle->counters()->count() > 0)
 
-@component('components.card')
-	@slot('title', 'Mantenimientos')
+	@component('components.card')
+		@slot('title', 'Mantenimientos Chasis')
 
-	<display-more>
-		<template v-slot:head>
-			@foreach($vehicle->counters->sortByDesc('completedPercent')->take(5) as $counter)
-				<div class="mb-5">@include('fleet.vehicles.counters.progress')</div>
-			@endforeach
-		</template>
-		<template v-slot:body>
-			@foreach($vehicle->counters->sortByDesc('completedPercent')->slice(5) as $counter)
-				<div class="mb-5">@include('fleet.vehicles.counters.progress')</div>
-			@endforeach
-		</template>
-	</display-more>
-	
-@endcomponent
+		<display-more>
+			<template v-slot:head>
+				@foreach($vehicle->counters->where('vehicle_category', 'chassis')->sortByDesc('completedPercent')->take(5) as $counter)
+					<div class="mb-5">@include('fleet.vehicles.counters.progress')</div>
+				@endforeach
+			</template>
+			<template v-slot:body>
+				@foreach($vehicle->counters->where('vehicle_category', 'chassis')->sortByDesc('completedPercent')->slice(5) as $counter)
+					<div class="mb-5">@include('fleet.vehicles.counters.progress')</div>
+				@endforeach
+			</template>
+		</display-more>
+		
+	@endcomponent
+
+	@component('components.card')
+		@slot('title', 'Mantenimientos Equipos')
+
+		<display-more>
+			<template v-slot:head>
+				@foreach($vehicle->counters->where('vehicle_category', 'equipment')->sortByDesc('completedPercent')->take(5) as $counter)
+					<div class="mb-5">@include('fleet.vehicles.counters.progress')</div>
+				@endforeach
+			</template>
+			<template v-slot:body>
+				@foreach($vehicle->counters->where('vehicle_category', 'equipment')->sortByDesc('completedPercent')->slice(5) as $counter)
+					<div class="mb-5">@include('fleet.vehicles.counters.progress')</div>
+				@endforeach
+			</template>
+		</display-more>
+		
+	@endcomponent
 
 @endif
