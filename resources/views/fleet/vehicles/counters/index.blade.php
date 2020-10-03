6a11b855f1da71e5732663b546d322a2edeeb896
Daniel Ramirez <dani.rg15@gmail.com>
2020-10-03 23:02:43 +0200

    split counters by type in vehicle

--- resources/views/fleet/vehicles/counters/index.blade.php
+++ resources/views/fleet/vehicles/counters/index.blade.php
@@ -9,6 +9,21 @@
 	@component('components.card')
 	  @slot('title', 'Contadores')
 
+	  @slot('corner')
+	  	<div class="flex">
+	  		<import-vehicle-counters class="mr-3"
+	  			:plans="{{ json_encode($vehicle->getMaintenancePlans()) }}"
+	  			:vehicle-id="{{$vehicle->id}}"
+	  			:current-counters="{{ json_encode($vehicle->counters) }}">
+	  		</import-vehicle-counters>
+	  		
+	  		<a href="{{ route('fleet.vehicles.counters.create', $vehicle) }}" class="btn-outline-gray flex items-center">
+	  			<i class="icon fas fa-plus-circle mr-2"></i>
+	  			Nuevo
+	  		</a>
+	  	</div>
+	  @endslot
+
 	  {!! Form::model($vehicle, [
 	  	'route' => ['fleet.vehicles.update', $vehicle],
 	  	'method' => 'PUT',
@@ -70,21 +85,7 @@
 	@endcomponent
 
 	@component('components.card', ['is_table' => true])
-		@slot('title', 'Mantenimientos')
-		@slot('corner')
-			<div class="flex">
-				<import-vehicle-counters class="mr-3"
-					:plans="{{ json_encode($vehicle->getMaintenancePlans()) }}"
-					:vehicle-id="{{$vehicle->id}}"
-					:current-counters="{{ json_encode($vehicle->counters) }}">
-				</import-vehicle-counters>
-				
-				<a href="{{ route('fleet.vehicles.counters.create', $vehicle) }}" class="btn-outline-gray flex items-center">
-					<i class="icon fas fa-plus-circle mr-2"></i>
-					Nuevo
-				</a>
-			</div>
-		@endslot
+		@slot('title', 'Mantenimientos Chasis')
 		<table>
 		  <thead>
 		    <tr>
@@ -95,17 +96,61 @@
 		    </tr>
 		  </thead>
 		  <tbody>
-		  	@foreach($vehicle->counters as $counter)
+		  	@foreach($vehicle->counters->where('vehicle_category', 'chassis') as $counter)
 		  	<tr>
 		  	  <td>
-		  	  	<span class="uppercase font-medium text-xs">
-		  	  		@if($counter->vehicle_category == 'chassis')
-		  	  			Chasis
-		  	  		@else
-		  	  			Equipo
-		  	  		@endif
-		  	  	</span>
-		  	  	&middot; {{ $counter->description }}
+		  	  	{{ $counter->description }}
+		  	  </td>
+		  	  <td>{{ round($counter->current, 2) }} ({{ $counter->completedPercent  }}%)</td>
+		  	  <td>
+		  	  	<strong>{{ $counter->max }}</strong>
+		  	  	@if($counter->type == 'work_hours')
+		  	  		H. Trabajo
+		  	  	@elseif($counter->type == 'natural_hours')
+		  	  		H. Naturales
+		  	  	@elseif($counter->type == 'kms')
+		  	  		Kms
+		  	  	@endif
+		  	  </td>
+		  	  <td>
+		  	  	<div class="flex">
+		  	  		<form method="POST" onsubmit="return confirmDelete()" action="{{ route('fleet.vehicles.counters.reset', [$vehicle, $counter]) }}">
+		  	  			@csrf
+		  	  			<button class="text-indigo-600 hover:text-indigo-900 focus:outline-none focus:underline mr-3">Reiniciar</button>
+		  	  		</form>
+
+		  	  		<a href="{{ route('fleet.vehicles.counters.edit', [$vehicle, $counter]) }}" class="mr-3">
+		  	  			<i class="icon fas fa-edit"></i>
+		  	  		</a>
+		  	  		<form method="POST" onsubmit="return confirmDelete()" action="{{ route('fleet.vehicles.counters.destroy', [$vehicle, $counter]) }}">
+		  	  			@csrf
+		  	  			@method('DELETE')
+		  	  			<button><i class="icon fas fa-trash-alt"></i></button>
+		  	  		</form>
+		  	  	</div>
+		  	  </td>
+		  	</tr>
+		  	@endforeach
+		  </tbody>
+		</table>
+	@endcomponent
+
+	@component('components.card', ['is_table' => true])
+		@slot('title', 'Mantenimientos Equipo')
+		<table>
+		  <thead>
+		    <tr>
+		      <th>Descripción</th>
+		      <th>Actual</th>
+		      <th>Máximo</th>
+		      <th></th>
+		    </tr>
+		  </thead>
+		  <tbody>
+		  	@foreach($vehicle->counters->where('vehicle_category', 'equipment') as $counter)
+		  	<tr>
+		  	  <td>
+		  	  	{{ $counter->description }}
 		  	  </td>
 		  	  <td>{{ round($counter->current, 2) }} ({{ $counter->completedPercent  }}%)</td>
 		  	  <td>
