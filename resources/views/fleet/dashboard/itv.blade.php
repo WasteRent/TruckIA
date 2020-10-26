2b73e74c886f012f48df3e2115bcdb17024bc9e8
Daniel Ramirez <dani.rg15@gmail.com>
2020-10-26 15:38:52 +0100

    group comming itvs by month

--- resources/views/fleet/dashboard/itv.blade.php
+++ resources/views/fleet/dashboard/itv.blade.php
@@ -36,13 +36,23 @@
 			Próximas
 		</h3>
 	</div>
-	<div class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
-		@foreach($comming as $vehicle) 	
-			@if(!$ongoing->contains($vehicle))
-				@include('fleet.dashboard.itv_card')
-			@endif
+	
+		@foreach($comming->groupBy(function($i) {
+			return Carbon\Carbon::parse($i->itv_date)->format('m');
+		}) as $month => $vehicles)
+
+			<div class="mt-4 text-indigo-800 font-medium">
+				{{ Carbon\Carbon::create()->day(1)->month($month)->format('M') }}
+			</div>
+
+			<div class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
+				@foreach($vehicles as $vehicle)
+					@if(!$ongoing->contains($vehicle))
+						@include('fleet.dashboard.itv_card')
+					@endif
+				@endforeach
+			</div>
 		@endforeach
-	</div>
 
 	<div class="mt-8">
 		<h3 class="text-lg leading-6 font-medium text-gray-900">
