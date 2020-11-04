980d335fe726c0c870cb9b912cfc33d53af4450c
YuriNekito <yuri.unic@gmail.com>
2020-11-04 06:18:11 +0100

    Reparo fallos detectados en la indentación

--- resources/views/fleet/garages/customers/index.blade.php
+++ resources/views/fleet/garages/customers/index.blade.php
@@ -14,34 +14,34 @@
 	@if(count($customers_search) > 0)
 		@component('components.card', ['is_table' => true])
 			@slot('title', 'Seleccionar cliente')
-            <table>
-                <thead>
-                  <tr>
-                    <th>Nombre</th>
-                    <th>Email</th>
-                    <th>Tel.</th>
-                    <th >Dirección</th>
-                    <th></th>
-                  </tr>
-                </thead>
-                <tbody>
-                    @foreach($customers_search as $customer)
-                    <tr>
-                        <td>{{$customer->name}} </td>
-                        <td>{{$customer->email1}}</td>
-                        <td>{{$customer->phone1}}</td>
-                        <td>{{$customer->full_address}}</td>
-			  	  <td>
-		  	  		<form method="POST" action="{{ route('fleet.garages.customers.store', $garage) }}">
-		  	  			@csrf
-		  	  			<input type="hidden" name="customer_id" value="{{$customer->id}}">
-		  	  			<button><i class="icon fas fa-plus-circle"></i></button>
-		  	  		</form>
-			  	  </td>
-			  	</tr>
-			  	@endforeach
-			  </tbody>
-			</table>
+        <table>
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Email</th>
+              <th>Tel.</th>
+              <th >Dirección</th>
+              <th></th>
+            </tr>
+          </thead>
+            <tbody>
+              @foreach($customers_search as $customer)
+              <tr>
+                <td>{{$customer->name}} </td>
+                <td>{{$customer->email1}}</td>
+                <td>{{$customer->phone1}}</td>
+                <td>{{$customer->full_address}}</td>
+			  	      <td>
+		  	  		    <form method="POST" action="{{ route('fleet.garages.customers.store', $garage) }}">
+		  	  			    @csrf
+		  	  			    <input type="hidden" name="customer_id" value="{{$customer->id}}">
+		  	  			    <button><i class="icon fas fa-plus-circle"></i></button>
+		  	  		    </form>
+			  	      </td>
+			        </tr>
+			  	    @endforeach
+			      </tbody>
+			  </table>
 		@endcomponent
 	@endif
 
@@ -51,33 +51,33 @@
 		@component('components.card', ['is_table' => true])
 			@slot('title', 'Clientes asignados')
 			<table>
-                <thead>
-                  <tr>
-                    <th>Nombre</th>
-                    <th>Email</th>
-                    <th>Tel.</th>
-                    <th >Dirección</th>
-                    <th></th>
-                  </tr>
-                </thead>
-                <tbody>
-                    @foreach($customers as $customer)
-                    <tr>
-                        <td>{{$customer->name}} </td>
-                        <td>{{$customer->email1}}</td>
-                        <td>{{$customer->phone1}}</td>
-                        <td>{{$customer->full_address}}</td>
-                        <td>
-                          <form method="POST" onsubmit="return confirmDelete()" action="{{ route('fleet.garages.customers.destroy', [$garage, $customer]) }}">
-                              @csrf
-                              @method('DELETE')
-                              <button class="btn-outline-red">Eliminar</button>
-                          </form>
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-              </table>
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Email</th>
+            <th>Tel.</th>
+            <th >Dirección</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          @foreach($customers as $customer)
+            <tr>
+                <td>{{$customer->name}} </td>
+                <td>{{$customer->email1}}</td>
+                <td>{{$customer->phone1}}</td>
+                <td>{{$customer->full_address}}</td>
+                <td>
+                  <form method="POST" onsubmit="return confirmDelete()" action="{{ route('fleet.garages.customers.destroy', [$garage, $customer]) }}">
+                      @csrf
+                      @method('DELETE')
+                      <button class="btn-outline-red">Eliminar</button>
+                  </form>
+                </td>
+            </tr>
+          @endforeach
+        </tbody>
+      </table>
 		@endcomponent
 	@endif
 @endsection
