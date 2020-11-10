d72ac26c18282ff7732589325d8f61612b349b24
Daniel Ramirez <dani.rg15@gmail.com>
2020-11-10 22:05:12 +0100

    update garage form

--- resources/views/fleet/garages/form.blade.php
+++ resources/views/fleet/garages/form.blade.php
@@ -1,23 +1,23 @@
 <div class="flex flex-wrap -mx-3 mb-6">
-  <div class="w-full md:w-2/4 px-3 mb-6 md:mb-0">
+  <div class="w-full md:w-5/12 px-3 mb-6 md:mb-0">
     <label class="form-label form-required">
       Nombre
     </label>
     {!! Form::text('name', null, ['class' => 'form-input']) !!}
   </div>
-  <div class="w-full md:w-1/4 px-3 mb-6 md:mb-0">
+  <div class="w-full md:w-2/12 px-3 mb-6 md:mb-0">
     <label class="form-label form-required">
       CIF
     </label>
     {!! Form::text('cif', null, ['class' => 'form-input']) !!}
   </div>
-  <div class="w-full md:w-1/4 px-3 mb-6 md:mb-0">
+  <div class="w-full md:w-4/12 px-3 mb-6 md:mb-0">
     <label class="form-label">
       Email notificaciones
     </label>
     {!! Form::email('notifications_email', null, ['class' => 'form-input']) !!}
   </div>
-  <div class="w-full md:w-1/6 px-3 mb-6 md:mb-0">
+  <div class="w-full md:w-1/12 px-3 mb-6 md:mb-0">
     <label class="form-label">
       Gestor
     </label>
