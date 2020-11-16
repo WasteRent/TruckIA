2dd3a0f3db487dc3437c6a99c7ae21c74795f1cc
YuriNekito <yuri.unic@gmail.com>
2020-11-16 01:10:19 +0100

    Añado los filtros de chasis y equipo a los buscadores, existe la posibilidad de cambiarle el estílo si se prefiere

--- resources/views/fleet/dashboard/search.blade.php
+++ resources/views/fleet/dashboard/search.blade.php
@@ -11,6 +11,30 @@
     </div>
     <div class="lg:px-3 sm:w-2/12 lg:mb-0 mb-3">
       <label class="form-label">
+        Marca Chasis
+      </label>
+        {!! Form::select('chassis_maker_id', $chassis_manufacturers->pluck('name', 'id')->prepend('', ''), null, ['class' => 'form-select', 'onchange' => "ajaxSelect('chassis_maker_id', 'chassis_model_id', '/api/manufacturer/{id}/models')"]) !!}
+    </div>
+    <div class="lg:px-3 sm:w-2/12 lg:mb-0 mb-3">
+      <label class="form-label">
+        Modelo Chasis
+      </label>
+        {!! Form::select('chassis_model_id', $chassis_models->pluck('name', 'id')->prepend('', ''), null, ['class' => 'form-select']) !!}
+    </div>
+    <div class="lg:px-3 sm:w-2/12 lg:mb-0 mb-3">
+      <label class="form-label">
+        Marca Equipo
+      </label>
+        {!! Form::select('equipment_maker_id', $equipment_manufacturers->pluck('name', 'id')->prepend('', ''), null, ['class' => 'form-select', 'onchange' => "ajaxSelect('equipment_maker_id', 'equipment_model_id', '/api/manufacturer/{id}/models')"]) !!}
+    </div>
+    <div class="lg:px-3 sm:w-2/12 lg:mb-0 mb-3">
+      <label class="form-label">
+        Modelo Equipo
+      </label>
+        {!! Form::select('equipment_model_id', $equipment_models->pluck('name', 'id')->prepend('', ''), null, ['class' => 'form-select']) !!}
+    </div>
+    <div class="lg:px-3 sm:w-2/12 lg:mb-0 mb-3">
+      <label class="form-label">
         Estado
       </label>
         {!! Form::select('state_id', $states->pluck('name', 'id')->prepend('', ''), null, ['class' => 'form-select']) !!}
