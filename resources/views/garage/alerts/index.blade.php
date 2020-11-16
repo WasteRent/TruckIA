a9c389f9b94aad736475d1ef45957141897cad46
YuriNekito <yuri.unic@gmail.com>
2020-11-16 01:19:43 +0100

    Estas dos, no tenían ningún "if" siquiera

--- resources/views/garage/alerts/index.blade.php
+++ resources/views/garage/alerts/index.blade.php
@@ -32,15 +32,20 @@
 				<td title="{{ $alert->created_at->format('d/m/Y H:i:s') }}">{{ $alert->created_at->diffForHumans() }}</td>
 				<td>
 					<div class="flex items-center">
-								<a href="{{ route('alert.linking', $alert) }}" class="mr-4">
-									<i class="fas fa-tools"></i>
-								</a>
+						@if($alert->action_url)
+							<a href="{{ route('alert.linking', $alert) }}" class="mr-4">
+								<i class="fas fa-tools"></i>
+							</a>
+						@endif	
+									
+						@if(!$alert->dismissed)
 							<form method="POST" action="{{ route('garage.alerts.update', $alert) }}">
 								@csrf
 								@method('PUT')
 								<input type="hidden" name="dismissed" value="1">
 								<button class="text-indigo-600 hover:text-indigo-900 focus:outline-none focus:underline">Descartar</button>
 							</form>
+						@endif
 					</div>
 				</td>
 		  	</tr>
