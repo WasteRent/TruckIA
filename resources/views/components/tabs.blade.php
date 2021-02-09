0fb371aca2fb17c2af4e1aaf7e4565ace07de578
YuriNekito <yuri.unic@gmail.com>
2021-02-09 05:34:08 +0100

    Habilito la función al módulo de Clientes en la página de Vehículo -> Detalles

--- resources/views/components/tabs.blade.php
+++ resources/views/components/tabs.blade.php
@@ -10,9 +10,11 @@
     <div class="border-b border-gray-200">
       <nav class="-mb-px flex">
       	@foreach($items as $item)
-        <a href="{{ $item['url'] }}" class="flex-1 py-4 px-1 text-center border-b-2 font-medium text-sm leading-5 focus:outline-none {{ $item['active'] ? 'border-indigo-500 text-indigo-600 focus:text-indigo-800 focus:border-indigo-700' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:text-gray-700 focus:border-gray-300' }}">
-          {!! $item['name'] !!}
-        </a>
+          @if($item['disponible'])
+            <a href="{{ $item['url'] }}" class="flex-1 py-4 px-1 text-center border-b-2 font-medium text-sm leading-5 focus:outline-none {{ $item['active'] ? 'border-indigo-500 text-indigo-600 focus:text-indigo-800 focus:border-indigo-700' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:text-gray-700 focus:border-gray-300' }}">
+            {!! $item['name'] !!}
+            </a>
+          @endif
         @endforeach
       </nav>
     </div>
