2d59c6267ef2387141ebe946fee153df54c08627
Daniel Ramirez <dani.rg15@gmail.com>
2021-03-15 14:31:20 +0100

    fix

--- resources/views/components/tabs.blade.php
+++ resources/views/components/tabs.blade.php
@@ -14,6 +14,10 @@
             <a href="{{ $item['url'] }}" class="flex-1 py-4 px-1 text-center border-b-2 font-medium text-sm leading-5 focus:outline-none {{ $item['active'] ? 'border-indigo-500 text-indigo-600 focus:text-indigo-800 focus:border-indigo-700' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:text-gray-700 focus:border-gray-300' }}">
             {!! $item['name'] !!}
             </a>
+          @elseif(!isset($item['available']))
+            <a href="{{ $item['url'] }}" class="flex-1 py-4 px-1 text-center border-b-2 font-medium text-sm leading-5 focus:outline-none {{ $item['active'] ? 'border-indigo-500 text-indigo-600 focus:text-indigo-800 focus:border-indigo-700' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:text-gray-700 focus:border-gray-300' }}">
+            {!! $item['name'] !!}
+            </a>
           @endif
         @endforeach
       </nav>
