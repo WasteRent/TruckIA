a3c5c6728b9ddb9c81396ad22ec0ff97843ee427
YuriNekito <yuri.unic@gmail.com>
2021-06-10 23:30:07 +0200

    se añaden las opciones de crear y editar a los usuarios en la pantalla de admin

--- app/Http/Controllers/Admin/AdminUserController.php
+++ app/Http/Controllers/Admin/AdminUserController.php
@@ -3,7 +3,12 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
+use App\Models\Fleet;
 use App\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AdminUserController extends Controller
 {
@@ -13,5 +18,56 @@ class AdminUserController extends Controller
         return view('admin.users.index', [
             'users' => User::paginate()
         ]);
+    }
+
+    public function create()
+    {
+        return view('admin.users.create');
+    }
+
+    public function store(StoreUserRequest $request)
+    {
+        User::create([
+            'name'      => $request->name,
+            'username'  => $request->username,
+            'password'  => Hash::make($request->input('password')),
+            'email'     => $request->email,
+            'is_active' => $request->boolean('is_active'),
+            'is_readonly' => $request->boolean('is_readonly'),
+            'role' => 'fleet',
+            'entity_relation_id' => Auth::user()->fleet->id
+        ]);
+        return redirect()->route('admin.users.index')->with('success_message', 'Usuario creado');
+    }
+
+    public function edit(User $user)
+    {
+        return view('admin.users.edit', [
+            'user' => $user
+        ]);
+    }
+
+    public function update(UpdateUserRequest $request, User $user)
+    {
+        if ($request->password) {
+            $user->password = bcrypt($request->password);
+            $user->save();
+        }
+        
+        $user->update([
+            'name'      => $request->name,
+            'username'  => $request->username,
+            'email'     => $request->email,
+            'is_active' => $request->boolean('is_active'),
+            'is_readonly' => $request->boolean('is_readonly')
+        ]);
+
+        return redirect()->route('admin.users.index')->with('success_message', 'Usuario actualizado');
+    }
+
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return back()->with('success_message', 'Usuario eliminado');
     }
 }
---
+++ resources/views/admin/users/create.blade.php
@@ -0,0 +1,66 @@
+@extends('layouts.admin')
+
+@section('content')
+  
+  @component('components.card')
+    @slot('title', 'Nuevo Usuario')
+
+      
+    {!! Form::open([
+      'route' => ['admin.users.store'],
+      'method' => 'POST',
+      'class' => 'w-full'
+    ]) !!}  
+
+      <div class="flex flex-wrap -mx-3 mb-6">
+        <div class="w-full md:w-1/5 px-3 mb-6 md:mb-0">
+          <label class="form-label form-required">
+            Nombre
+          </label>
+          {!! Form::text('name', null, ['class' => 'form-input']) !!}
+        </div>
+        <div class="w-full md:w-1/5 px-3 mb-6 md:mb-0">
+          <label class="form-label form-required">
+            Usuario
+          </label>
+          {!! Form::text('username', null, ['class' => 'form-input']) !!}
+        </div>
+        <div class="w-full md:w-1/5 px-3 mb-6 md:mb-0">
+          <label class="form-label form-required">
+            Contraseña
+          </label>
+          {!! Form::text('password', null, ['class' => 'form-input']) !!}
+        </div>
+        <div class="w-full md:w-1/5 px-3 mb-6 md:mb-0">
+          <label class="form-label form-required">
+            Email
+          </label>
+          {!! Form::email('email', null, ['class' => 'form-input']) !!}
+        </div>
+      </div>
+      <div class="flex flex-wrap -mx-3 mb-6">
+        <div class="w-full md:w-1/5 px-3 mb-6 md:mb-0">
+          <label class="form-label">
+            Activo
+          </label>
+          {!!  Form::checkbox('is_active', 1, true)  !!}
+        </div>
+        <div class="w-full md:w-1/5 px-3 mb-6 md:mb-0">
+          <label class="form-label">
+            Sólo permisos de lectura
+          </label>
+          {!!  Form::checkbox('is_readonly', 1)  !!}
+        </div>
+      </div>
+
+      <div class="flex justify-end">
+        <button class="btn-indigo">Guardar</button>
+      </div>
+
+    {!! Form::close() !!}
+
+
+  @endcomponent
+
+@endsection
+
