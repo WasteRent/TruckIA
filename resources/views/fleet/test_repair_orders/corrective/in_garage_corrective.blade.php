aee4cbaeb8fd256bb3b999dfec5e426dfe206f14
yuri_aristegui <yuri.unic@gmail.com>
2021-07-08 00:53:39 +0200

    Se aplican los cambios para hacer update y para la pantalla de estado en taller correctivo

--- resources/views/fleet/test_repair_orders/corrective/in_garage_corrective.blade.php
+++ resources/views/fleet/test_repair_orders/corrective/in_garage_corrective.blade.php
@@ -5,71 +5,65 @@
 @section('content')
 
 @component('components.card')
-
-  {!! Form::open([
-    'route' => ['fleet.garages.store'],
-    'method' => 'POST',
-    'class' => 'w-full'
-  ]) !!}	
-	@include('fleet.test_repair_orders.form')
-  {!! Form::close() !!}
+  {!! Form::model($repair_order, [
+			'route' => ['fleet.test-repair-orders.update', $repair_order],
+			'method' => 'PUT',
+			'class' => 'w-full'
+		]) !!}	
+	
+	@include('fleet.test_repair_orders.form', $vehicle)
 
   {{-- En esta sección solo se modifica los datos de un OR creado en un taller externo y que no dispone de fecha de salida --}}
   <div class="container">
-    <form onsubmit="return confirmAction()" class="mr-4" method="POST" action="{{ route('fleet.test-repair-orders.update') }}">
-        @csrf
-        @method('PUT')
 
         <h2>DESCRIPCIÓN</h2>
 
         <div class="mt-5 flex">
           <div class="w-full md:w-4/12 px-3 mb-6 md:mb-0">
-              {!! Form::text('fleet', 'URBAN TRUCKS', ['class' => 'form-input']) !!}
+              {!! Form::text('garage_id', $repair_order->garage->name, ['class' => 'form-input', 'disabled']) !!}
           </div>
           <div class="w-full md:w-4/12 px-3 mb-6 md:mb-0">
-              {!! Form::text('entry_date', '(Fecha de entrada)', ['class' => 'form-input']) !!}
+              {!! Form::text('workshop_date', $repair_order->workshop_date, ['class' => 'form-input datepicker', 'disabled']) !!}
           </div>
           <div class="w-full md:w-4/12 px-3 mb-6 md:mb-0">
-              {!! Form::text('exit_date', '(Fecha de salida)', ['class' => 'form-input']) !!}
+              {!! Form::text('identificator', $repair_order->identificator, ['class' => 'form-input', 'placeholder' => '(Nº de pedido / O.R.)']) !!}
           </div>
         </div>
 
         <div class="mt-5 flex w-full">
           <div class="w-full md:w-2/12 px-3 mb-6 md:mb-0">
-            {!! Form::text('equipo', 'Equipo', ['class' => 'form-input']) !!}
+            {!! Form::select('operation_family',['chassis' => 'Chasis', 'equipment' => 'Equipo', 'both' => 'Ambas', 'unknow' => 'Desconocido'] ,$repair_order->operation_family, ['class' => 'form-select', 'disabled']) !!}
           </div>
           <div class="w-full md:w-2/12 px-3 mb-6 md:mb-0">
-            {!! Form::text('tipo_de_equipo', 'Hidráulica', ['class' => 'form-input']) !!}
+            {!! Form::select('operation_subfamily',['hidraulic' => 'Hidráulica', 'mechanic' => 'Mecánica', 'electrical' => 'Eléctrica', 'pneumatics' => 'Neumática', 'unknow' => 'Desconocido', 'others' => 'Otros'] ,$repair_order->operation_subfamily, ['class' => 'form-select', 'disabled']) !!}
           </div>
           <div class="w-full md:w-4/12 px-3 mb-6 md:mb-0">
-            {!! Form::label('Siniestro','¿Se trata de un siniestro?:') !!}
-            SI {!! Form::radio('Siniestro', '1', '') !!}
-            NO {!! Form::radio('Siniestro', '0', 'false') !!}
+            {!! Form::label('sinister','¿Se trata de un siniestro?:') !!}
+            {!! Form::select('sinister', ['1' => 'SI', '0' => 'NO'], $repair_order->sinister, ['disabled']) !!}
           </div>
           <div class="w-full md:w-4/12 px-3 mb-6 md:mb-0">
-            {!! Form::label('Malos_Usos','¿Intervención por malos usos / golpes?:') !!}
-            SI {!! Form::radio('Malos_Usos', '1', '') !!}
-            NO {!! Form::radio('Malos_Usos', '0', 'false') !!}
+            {!! Form::label('missuse','¿Intervención por malos usos / golpes?:') !!}
+            {!! Form::select('misuse', ['1' => 'SI', '0' => 'NO'], $repair_order->misuse, ['disabled']) !!}
           </div>
         </div>
         <div class="mt-5 flex w-full">
           <div class="w-full px-3 mb-6 md:mb-0">
-            {!! Form::textarea('description', '(CAUSA) p.ej.: La prensa no tiene fuerza.', ['class' => 'form-input']) !!}
+            {!! Form::textarea('description', $repair_order->description, ['class' => 'form-input', 'disabled']) !!}
           </div>
         </div>
         
         <h2>¿YA HA SALIDO DE TALLER?</h2>
         <div class="w-full">
           <div class="w-full md:w-2/12 px-3 mb-6 md:mb-0">
-            {!! Form::text('date', '2021-01-01', ['class' => 'form-input datepicker']) !!}
+              {!! Form::text('workshop_exit_date', $repair_order->workshop_exit_date, ['class' => 'form-input datepicker']) !!}
           </div>
         </div>
 
         <button class="btn-indigo float-right mb-2">
           Guardar
         </button>
-    </form>
   </div>
+		{!! Form::close() !!}
 
 @endcomponent
 @endsection
