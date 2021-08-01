107eb80e21023785671e9aeb55104e2eb21aa1b8
Daniel Ramirez <dani.rg15@gmail.com>
2021-08-01 18:20:12 +0200

    fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -57,7 +57,7 @@
 	          From: "opacity-100 scale-100"
 	          To: "opacity-0 scale-95"
 	      -->
-	      <div class="absolute top-0 inset-x-0 p-2 transition transform origin-top md:hidden">
+	      <div class="absolute top-0 inset-x-0 p-2 transition transform origin-top hidden">
 	        <div class="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
 	          <div class="px-5 pt-4 flex items-center justify-between">
 	            <div>
