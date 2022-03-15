6b4573b1070356945c2bbece8ada30402f0219aa
Daniel Ramirez <dani.rg15@gmail.com>
2022-03-15 19:10:37 +0100

    fix

--- resources/views/fleet/customers/garages/index.blade.php
+++ resources/views/fleet/customers/garages/index.blade.php
@@ -65,7 +65,7 @@
 			  <tbody>
 			  	@foreach($customer->garages as $garage)
 			  	<tr >
-			  	  <td>{{$garage->name}} </td>
+			  	  <td><a class="text-indigo-600 hover:text-indigo-900 focus:outline-none focus:underline" href="{{ route('fleet.garages.show', $garage) }}">{{$garage->name}}</a></td>
 			  	  <td>{{$garage->email}}</td>
 			  	  <td>{{$garage->phone}}</td>
 			  	  <td>{{$garage->full_address}}</td>
