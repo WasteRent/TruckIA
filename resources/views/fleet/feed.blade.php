c92a83863b66ff538d8ef074f2fe6f6cbe9be9c8
Daniel Ramirez <dani.rg15@gmail.com>
2022-03-21 08:56:24 +0100

    fix

--- resources/views/fleet/feed.blade.php
+++ resources/views/fleet/feed.blade.php
@@ -14,8 +14,8 @@
             <div class="relative flex items-start space-x-3">
               <div class="relative">
 
-                @if(Auth::user()->avatar)
-                  <img loading="lazy" class="h-10 w-10 rounded-full bg-gray-400 flex items-center justify-center ring-8 ring-white" src="{{ Auth::user()->avatar->getLink() }}"/>
+                @if($item->user)
+                  <img loading="lazy" class="h-10 w-10 rounded-full bg-gray-400 flex items-center justify-center ring-8 ring-white" src="{{ $item->user->avatar->getLink() }}"/>
                 @else
                   <svg class="h-10 w-10 rounded-full bg-gray-400 flex items-center justify-center ring-8 ring-white" fill="currentColor" viewBox="0 0 24 24">
                     <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
