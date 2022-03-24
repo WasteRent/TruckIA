b14414cb38b1efa667925004c3e09714717e48bb
Daniel Ramirez <dani.rg15@gmail.com>
2022-03-24 11:05:26 +0100

    Revert "fix"
    This reverts commit ecc015894c96877862c6afe8ba014ff254399229.

--- app/Http/Controllers/Fleet/FleetIncidentController.php
+++ app/Http/Controllers/Fleet/FleetIncidentController.php
@@ -12,12 +12,12 @@ class FleetIncidentController extends Controller
 
     public function index(Request $request)
     {
-        $users = VehicleIncident::whereNull('closed_at')->where('fleet_id', Auth::user()->fleet->id)->get()->map(function($incident) {
+        $users = VehicleIncident::whereNull('closed_at')->get()->map(function($incident) {
             return $incident->user;
         })->unique();
 
         return view('fleet.incidents.index', [
-            'incidents' => VehicleIncident::filter($request->toArray())->whereNull('closed_at')->where('fleet_id', Auth::user()->fleet->id)->latest()->get(),
+            'incidents' => VehicleIncident::filter($request->toArray())->whereNull('closed_at')->latest()->get(),
             'users' => $users
         ]);
     }
