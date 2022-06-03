34b8f57bc855cfb971cab9af466ec9b8319c4a32
Daniel Ramirez <dani.rg15@gmail.com>
2022-06-03 10:44:05 +0200

    requerid email to create fleet

--- app/Http/Requests/Admin/FleetRequest.php
+++ app/Http/Requests/Admin/FleetRequest.php
@@ -24,7 +24,8 @@ class FleetRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required'
+            'name' => 'required',
+            'email' => 'required|email'
         ];
     }
 
