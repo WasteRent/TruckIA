d9067dde1f8e807f5cee11985d2c09e06b6c2068
Daniel Ramirez <dani.rg15@gmail.com>
2022-06-30 14:50:20 +0200

    fix

--- resources/views/box/auth.blade.php
+++ resources/views/box/auth.blade.php
@@ -71,7 +71,7 @@
 	            <div>
 	              <label for="email" class="block text-sm font-medium text-gray-700">Matrícula</label>
 	              <div class="mt-1">
-	                <input name="plate" type="text" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-sky-500 focus:border-sky-500 sm:text-sm">
+	                <input name="plate" type="text" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-sky-500 focus:border-sky-500 sm:text-sm" value="{{ request()->query('plate') }}">
 	              </div>
 	            </div>
 
