bd305e4c1c19a890ba3ef9afaccb95248100bf42
Daniel Ramirez <dani.rg15@gmail.com>
2022-10-11 12:14:33 +0200

    improve

--- resources/views/box/auth.blade.php
+++ resources/views/box/auth.blade.php
@@ -19,6 +19,7 @@
 
           <input type="hidden" name="qrid" value="{{ request()->query('qrid') }}">
 
+
         	@if(isset($vehicle))
 	          <div class="mb-4">
 	            {!! Form::text('', request()->query('qrid') . " / {$vehicle->plate} " . $vehicle->chassis, ['disabled' => 1, 'class' => "py-5 px-7 rounded-md border-0 outter-shadow w-full text-sm"]) !!}
@@ -52,8 +53,10 @@
 	            @endif
 	          </div>
 	          <div class="mt-6">
-	            <button class="bg-teal-500 text-white px-10 py-2 rounded" type="submit">
-	              {{ __('Entrar') }}
+	            <button class="bg-teal-500 text-white px-10 py-2 rounded flex" type="submit">
+	              <span class="mr-2">{{ __('Entrar') }}</span>
+
+	              <svg class="w-6 h-6 text-teal-200" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M48 32C21.5 32 0 53.5 0 80v96c0 26.5 21.5 48 48 48h96c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48H48zm80 64v64H64V96h64zM48 288c-26.5 0-48 21.5-48 48v96c0 26.5 21.5 48 48 48h96c26.5 0 48-21.5 48-48V336c0-26.5-21.5-48-48-48H48zm80 64v64H64V352h64zM256 80v96c0 26.5 21.5 48 48 48h96c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48H304c-26.5 0-48 21.5-48 48zm64 16h64v64H320V96zm32 352v32h32V448H352zm96 0H416v32h32V448zM416 288v32H352V288H256v96 96h64V384h32v32h96V352 320 288H416z"/></svg>
 	            </button>
 	          </div>
 	        @else
