4653fcf1f3e59ed02a44f0ac21c44839546e2e89
Daniel Ramirez <dani.rg15@gmail.com>
2022-10-18 09:07:36 +0200

    remove odoo

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -62,7 +62,7 @@ class Kernel extends ConsoleKernel
         $schedule->job(new GenerateDailyCustomerPreventivesJob)->dailyAt('08:00');
         $schedule->job(new GenerateWeeklyCustomerPreventivesJob)->thursdays()->at('08:00');
 
-        $schedule->command('odoo:sync')->hourly();
+        //$schedule->command('odoo:sync')->hourly();
     }
 
     /**
