35abbd0ab6d99884798f9e5af6e1f5ec3620b28f
Daniel Ramirez <dani.rg15@gmail.com>
2022-10-24 14:40:13 +0200

    fix

--- resources/views/box/preview.blade.php
+++ resources/views/box/preview.blade.php
@@ -12,12 +12,12 @@
         <iframe class="rounded w-full h-64" src="https://www.youtube.com/embed/RTdkEKC1Mjg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
         <div class="flex justify-between space-x-3 mt-10">
-        	<a target="_blank" href="https://truckts.com/contacto" class="bg-teal-500 text-white px-10 py-2 rounded flex items-center" type="submit">
+        	<a target="_blank" href="https://truckts.com/contacto" class="bg-teal-500 text-white px-4 text-center py-2 rounded flex items-center" type="submit">
         	  <span class="mr-2">{{ __('Contactar') }}</span>
         	</a>
 
         	@if($vehicle)
-        	<a href="{{ route('box.login') }}?qrid={{ request()->query('qrid') }}" class=" border-2 border-teal-500 text-teal-500 px-10 py-2 rounded flex items-center" type="submit">
+        	<a href="{{ route('box.login') }}?qrid={{ request()->query('qrid') }}" class=" border-2 border-teal-500 text-teal-500 px-4 text-center py-2 rounded flex items-center" type="submit">
         	  <span class="mr-2">{{ __('Soy cliente, ir al vehículo') }}</span>
         	</a>
         	@endif
