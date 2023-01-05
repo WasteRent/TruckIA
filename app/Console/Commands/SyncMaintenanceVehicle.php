4a97fcf252c6107b5f9dfc4d958c704d2cf3e8a0
Daniel Ramirez <dani.rg15@gmail.com>
2023-01-05 16:40:25 +0100

    add pablot to swtich

--- app/Console/Commands/SyncMaintenanceVehicle.php
+++ app/Console/Commands/SyncMaintenanceVehicle.php
@@ -32,29 +32,47 @@ class SyncMaintenanceVehicle extends Command
         $vehicles = $this->argument('vehicle') ? Vehicle::whereId($this->argument('vehicle'))->get() : Vehicle::all();
 
         foreach($vehicles as $vehicle) {
-            $last_prev = $vehicle->repairOrders()->where('type', 'preventive')->where('state_id', RepairOrderState::FINISHED)->latest()->first();
-
-            if ($last_prev) {
-                echo "veh: $vehicle->plate OR $last_prev->id \n";
-
-                $operations = $last_prev->operations()->pluck('maintenance_plan_id')->unique()->toArray();
-                $chassis_diff = max($vehicle->chassis_can_work_hours - $last_prev->work_hours_chassis, 0);
-                $equipment_diff = max($vehicle->equipment_work_hours - $last_prev->work_hours_equipment, 0);
-
-                foreach ($vehicle->counters()->whereIn('type', ['work_hours', 'kms'])->get() as $counter) {
-                    if (in_array($counter->plan_id, $operations)) {
-                        if($counter->vehicle_category == 'chassis') {
-                            $counter->update(['current' => $chassis_diff]);
-                            echo "chassis $counter->id : $chassis_diff\n";
-                        } elseif($counter->vehicle_category == 'equipment') {
-                            $counter->update(['current' => $equipment_diff]);
-                            echo "equipment $counter->id : $equipment_diff\n";
-                        }
-                    }
-                }
-
-                echo "--------------------\n";
+            foreach ($vehicle->counters as $counter) {
+                $this->sync($vehicle, $counter);
             }
         }
+    }
+
+    private function sync(Vehicle $vehicle, $counter) {
+        $last_prev = $vehicle->repairOrders()
+                        ->where('type', 'preventive')
+                        ->where('state_id', RepairOrderState::FINISHED)
+                        ->whereHas('operations', function($q) use ($counter) {
+                            $q->where('maintenance_plan_id', $counter->plan_id);
+                        })
+                        ->latest()
+                        ->first();
+
+        if ($counter->vehicle_category == 'equipment' && $counter->type == 'work_hours') {
+            $value = $last_prev 
+                        ? max($vehicle->equipment_work_hours - $last_prev->work_hours_equipment, 0)
+                        : $vehicle->equipment_work_hours;
+            echo " - Equipment $counter->type $counter->max : $value\n";
+        }
+        else if ($counter->vehicle_category == 'chassis' && $counter->type == 'work_hours') {
+            $value = $last_prev
+                        ? max($vehicle->chassis_can_work_hours - $last_prev->work_hours_chassis, 0)
+                        : $vehicle->chassis_can_work_hours;
+            echo " - Chassis $counter->type $counter->max : $value\n";
+        }
+        else if ($counter->vehicle_category == 'chassis' && $counter->type == 'kms') {
+            $value = $last_prev 
+                        ? max($vehicle->kms - $last_prev->kms, 0)
+                        : $vehicle->kms;
+            echo " - Chassis $counter->type $counter->max : $value\n";
+        }
+        else if ($counter->vehicle_category == 'chassis' && $counter->type == 'natural_hours') {
+            $value = $last_prev 
+                        ? max($last_prev->created_at->diffInHours(), 0)
+                        : $counter->current;
+            echo " - Chassis $counter->type $counter->max : $value\n";
+        }
+
+        $counter->update(['current' => $value]);
     }
 }
