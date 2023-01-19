858783cba8c8d24c83c20698efdaec1e9ba9ed17
Daniel Ramirez <dani.rg15@gmail.com>
2023-01-19 10:51:20 +0100

    fix

--- resources/views/fleet/dashboard/fleet/index.blade.php
+++ resources/views/fleet/dashboard/fleet/index.blade.php
@@ -6,7 +6,6 @@
 
 	@include('fleet.dashboard.tabs', ['fleet' => true])
 
-	@if(auth()->user()->fleet->id == 1)
 		<div class="sm:grid grid-cols-4 gap-4">
 			<div class="col-span-3 flex">
 				@component('components.card')
@@ -44,37 +43,5 @@
 				@include('fleet.dashboard.fleet.recent_activity')
 			</div>
 		</div>
-	@else
-		<div class="sm:grid grid-cols-3 gap-4">
-			<div class="col-span-1">
-				@component('components.card')
-					@include('fleet.dashboard.fleet.charts.maintenance')
-				@endcomponent
-			</div>
-			<div class="col-span-1">
-				@component('components.card')
-					@include('fleet.dashboard.fleet.charts.mechanic')
-				@endcomponent
-			</div>
-			<div class="col-span-1">
-				@component('components.card')
-					@include('fleet.dashboard.fleet.charts.age')
-				@endcomponent
-			</div>
-
-			<div class="col-span-3">
-				@include('fleet.dashboard.fleet.recent_orders')
-			</div>
-			<div class="col-span-1 flex">
-				@include('fleet.dashboard.fleet.recent_alerts')
-			</div>
-			<div class="col-span-1 flex">
-				@include('fleet.dashboard.fleet.recent_incidents')
-			</div>
-			<div class="col-span-1 flex">
-				@include('fleet.dashboard.fleet.recent_activity')
-			</div>
-		</div>
-	@endif
 	
 @endsection
