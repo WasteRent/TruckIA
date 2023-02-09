7053e6d778d45393b9ed46d5b0b174ce8ed4dc1e
Daniel Ramirez <dani.rg15@gmail.com>
2023-02-09 10:43:20 +0100

    fix

--- resources/views/fleet/calendar/index.blade.php
+++ resources/views/fleet/calendar/index.blade.php
@@ -38,7 +38,7 @@
                     <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
                   </svg>
                 </dt>
-                <dd>{{ $appointment->order->vehicle->customer->name }}</dd>
+                <dd>{{ optional($appointment->order->vehicle->customer)->name }}</dd>
               </div>
               <div class="mt-2 flex items-start space-x-3 xl:mt-0 xl:ml-3.5 xl:border-l xl:border-gray-400 xl:border-opacity-50 xl:pl-3.5">
                 <dt class="mt-0.5">
