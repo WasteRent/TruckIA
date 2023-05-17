eb5282571bbb2651c146026090896ef6e98ac4ee
Daniel Ramirez <dani.rg15@gmail.com>
2023-05-17 08:51:53 +0200

    Revert "restricted operations on vehicles"
    This reverts commit 839c5d6354091823a33042ceeafbacde80847f9a.

--- app/Http/Requests/Fleet/VehicleRequest.php
+++ app/Http/Requests/Fleet/VehicleRequest.php
@@ -11,14 +11,7 @@ class VehicleRequest extends BaseFleetRequest
      */
     public function authorize()
     {
-        $allowed_users = [
-            892, //beatriz
-            928, //vvictor
-            955, //luciano
-            637, //yo
-            951, //jgarciasoto
-        ];
-        return ! auth()->user()->is_readonly && in_array(auth()->id(), $allowed_users);
+        return ! auth()->user()->is_readonly;
     }
 
     /**
