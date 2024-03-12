0dc4a67179e685507083a96b3010d4f279784a68
Daniel Ramirez <dani.rg15@gmail.com>
2024-03-12 09:20:52 +0100

    fix

--- resources/views/layouts/fleet.blade.php
+++ resources/views/layouts/fleet.blade.php
@@ -146,7 +146,7 @@
 			unset($enlaces[$key]);
 		}
 	}
-	if(in_array(Auth::user()->id, [3,920, 929,872,637, 928,955,904,1034])) {
+	if(in_array(Auth::user()->id, [3,920, 929,872,637, 928,955,904,1034,1033])) {
 		$enlaces[] =
 				[
 					'name' => __('Marcas y Modelos'),  
