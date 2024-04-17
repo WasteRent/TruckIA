b8a3e8ee3a638875cb231e9e378921fae47368bd
Daniel Ramirez <dani.rg15@gmail.com>
2024-04-17 12:09:08 +0200

    fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -64,7 +64,7 @@ class Kernel extends ConsoleKernel
         $schedule->job(new GenerateDailyCustomerPreventivesJob)->dailyAt('08:00');
         $schedule->job(new GenerateWeeklyCustomerPreventivesJob)->thursdays()->at('08:00');
 
-        $schedule->command('maintenance:sync')->hourly();
+        $schedule->command('maintenance:sync')->everyFifteenMinutes();
 
         $schedule->command('vehicles:import-from-odoo')->dailyAt('06:00');
 
