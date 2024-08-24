b7113cab3a9a90aded0f964b630fb34cac7982e9
Angel Castillejo <angel.castillejo.freelance@gmail.com>
2024-08-24 18:51:35 +0200

    fix

--- resources/views/fleet/incidents/create.blade.php
+++ resources/views/fleet/incidents/create.blade.php
@@ -19,7 +19,7 @@
           <label class="form-label form-required">
             {{ __('Incidencia') }}
           </label>
-          <x-trix name="incidence"></x-trix>
+          <textarea name="incidence" cols="30" rows="10"></textarea>
         </div>
         <div class="sm:w-1/3 px-3 mb-6 md:mb-0">
           <label class="form-label form-required">
