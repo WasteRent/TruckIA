2256ec5ba4f55679d378d0e5367869a9c637d4c0
Daniel Ramirez <dani.rg15@gmail.com>
2024-11-28 10:01:59 +0100

    fix

--- app/Http/Controllers/Fleet/FleetRepairOrdersController.php
+++ app/Http/Controllers/Fleet/FleetRepairOrdersController.php
@@ -83,7 +83,7 @@ class FleetRepairOrdersController extends Controller
 
     public function create(Request $request)
     {
-        $this->authorize('create');
+        $this->authorize('create', RepairOrder::class);
 
         if ($request->query('vehicle_id')) {
             session(['vehicle' => Vehicle::findOrFail($request->query('vehicle_id'))]);
