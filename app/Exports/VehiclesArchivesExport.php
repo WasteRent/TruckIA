007b85ebc2104675bce4a44301a40355c42150d0
Saul Romero <sromero@zululabs.es>
2025-01-31 11:22:34 +0100

    modificada la exportación ha pdf

--- app/Exports/VehiclesArchivesExport.php
+++ app/Exports/VehiclesArchivesExport.php
@@ -2,7 +2,9 @@
 
 namespace App\Exports;
 
-use App\Models\Vehicle;
+use App\Models\VehicleChecklistFile;
+use App\Models\VehicleChecklistFileType;
+use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithMapping;
@@ -10,32 +12,40 @@ use Maatwebsite\Excel\Concerns\WithMapping;
 class VehiclesArchivesExport implements FromCollection, WithHeadings, WithMapping
 {
     protected $request;
+    protected $fileTypes;
 
     public function __construct($request)
     {
         $this->request = $request;
-       
+        $this->fileTypes = VehicleChecklistFileType::pluck('name')->toArray();
     }
 
     public function collection()
     {
-        return Vehicle::where('id', $this->request->vehicle)->get();
+        return DB::table('vehicles as v')
+            ->join('vehicle_checklist_files as vf', 'v.id', '=', 'vf.vehicle_id')
+            ->join('vehicle_checklist_file_types as vt', 'vf.vehicle_checklist_file_type_id', '=', 'vt.id')
+            ->where('v.id', $this->request->vehicle)
+            ->select('v.plate', 'vt.name', 'vf.is_checked')
+            ->get()
+            ->groupBy('plate');
     }
 
     public function headings(): array
     {
-        return [
-            'Matrícula', 'Ficha técnica', 'Permiso de circulación', 'Manual de equipo',
-        ];
+        return array_merge(['Matrícula'], $this->fileTypes);
     }
 
     public function map($vehicle): array
     {
-        return [
-            $vehicle->plate,
-            $vehicle->vehicleChecklistFiles->technical_sheet ? 'Sí' : 'No',
-            $vehicle->vehicleChecklistFiles->vehicle_registration ? 'Sí' : 'No',
-            $vehicle->vehicleChecklistFiles->equipment_manual ? 'Sí' : 'No',
-        ];
+        $plate = $vehicle->first()->plate;
+        $fileData = $vehicle->pluck('is_checked', 'name')->toArray(); 
+
+        $mappedData = [$plate];
+        foreach ($this->fileTypes as $type) {
+            $mappedData[] = $fileData[$type] ? 'Sí' : 'No';
+        }
+
+        return $mappedData;
     }
 }
