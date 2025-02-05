d53e914eb75d42ed76c3a24ed8b1284173ba933b
Saul Romero <sromero@zululabs.es>
2025-02-05 12:37:30 +0100

    fix query y añadida ubicacion

--- app/Exports/VehiclesArchivesExport.php
+++ app/Exports/VehiclesArchivesExport.php
@@ -24,45 +24,24 @@ class VehiclesArchivesExport implements FromCollection, WithHeadings, WithMappin
     public function collection()
     {
 
-        return DB::table('vehicles as v')
-            ->leftJoin('vehicle_checklist_files as vf', 'v.id', '=', 'vf.vehicle_id')
-            ->leftJoin('vehicle_checklist_file_types as vt', 'vf.vehicle_checklist_file_type_id', '=', 'vt.id')
-            ->select('v.plate', 'vt.name', 'vf.is_checked')
-            ->whereNull('v.deleted_at')
-            ->where('v.fleet_id', 1)
-            ->get()
-            ->groupBy('plate');
+        return Vehicle::filter($this->request->all())->allowForUser()->get();
     }
 
     public function headings(): array
     {
-        return array_merge(['Matrícula'], $this->fileTypes);
+        return array_merge(['Matrícula'], ["Ubicación"], $this->fileTypes);
     }
 
-    public function map($vehicles): array
+    public function map($vehicle): array
     {
-      
-        $groupedVehicles = [];
+        $plate = $vehicle->plate;
+        $location = $vehicle->location->name ?? '';
+        $fileData = [];
 
-        // Agrupar documentos por número de matrícula (plate)
-        foreach ($vehicles as $vehicle) {
-            $plate = $vehicle->plate;
-            $groupedVehicles[$plate][$vehicle->name] = $vehicle->is_checked;
+        foreach ($this->fileTypes as $type) {
+            $fileData[] = isset($vehicle->$type) && $vehicle->$type ? 'Sí' : 'No';
         }
 
-        $result = [];
-
-        // Crear la estructura de salida
-        foreach ($groupedVehicles as $plate => $fileData) {
-            $mappedData = [$plate];
-
-            foreach ($this->fileTypes as $type) {
-                $mappedData[] = isset($fileData[$type]) && $fileData[$type] ? 'Sí' : 'No';
-            }
-
-            $result[] = $mappedData;
-        }
-
-        return $result;
+        return array_merge([$plate], [$location], $fileData);
     }
 }
