873766f5e28e1600f6586856f5edb73c5820f9da
Daniel Ramirez <dani.rg15@gmail.com>
2025-04-21 11:29:03 +0200

    add some drivers to lavados

--- resources/views/layouts/fleet.blade.php
+++ resources/views/layouts/fleet.blade.php
@@ -68,7 +68,7 @@
 		'icon' => '<i class="fas fa-water mr-2 w-4"></i>', 
 		'link' => route('fleet.washing.index'), 
 		'active' => request()->is('fleet/washing*'),
-		'disponible' => in_array(auth()->user()->job, ['fleet_manager', 'vehicle_washing', 'garage_boss', 'mechanic'])
+		'disponible' => in_array(auth()->user()->job, ['fleet_manager', 'vehicle_washing', 'garage_boss', 'mechanic']) || in_array(auth()->user()->username, ['victor1270', 'manuel1284'])
 	];
 
 	$enlaces[] =
