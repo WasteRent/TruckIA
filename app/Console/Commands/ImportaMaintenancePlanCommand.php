fe9729bfbf948dced1c33f3ca6dbff84737af9aa
Daniel Ramirez <dani.rg15@gmail.com>
2025-08-04 18:39:10 +0200

    import maintenances

--- app/Console/Commands/ImportaMaintenancePlanCommand.php
+++ app/Console/Commands/ImportaMaintenancePlanCommand.php
@@ -10,6 +10,8 @@ use App\Models\Vehicle;
 use App\Models\VehicleWorkCounter;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Artisan;
+
 
 class ImportaMaintenancePlanCommand extends Command
 {
@@ -18,7 +20,7 @@ class ImportaMaintenancePlanCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'maintenance-plan:import {fleet_id} {prefix_plan_name} {plates} {vehicle_category} {period_type} {file}';
+    protected $signature = 'maintenance-plan:import {fleet_id} {plates} {prefix_plan_name}';
 
     /**
      * The console command description.
@@ -34,51 +36,67 @@ class ImportaMaintenancePlanCommand extends Command
      */
     public function handle()
     {
-        DB::beginTransaction();
-
         $plates = explode(',', $this->argument('plates'));
         $fleet = Fleet::find($this->argument('fleet_id'));
+        $plans = MaintenancePlan::whereHas('fleet', function($query) use ($fleet) {
+            $query->where('fleet_id', $fleet->id);
+        })->where('name', 'LIKE', $this->argument('prefix_plan_name').'%')->get();
+
+        $this->info($plans->count());
+
+        if($plans->count() == 0) {
+            $this->error('No se encontraron planes');
+            return;
+        }
 
         try {
-            $vehicles = [];
+            DB::beginTransaction();
+
             foreach ($plates as $plate) {
-                $vehicles[] = Vehicle::where('plate', $plate)->where('fleet_id', $fleet->id)->first();
-            }
+                $vehicle = Vehicle::where('plate', $plate)->where('fleet_id', $fleet->id)->firstOrFail();
 
-            $operations = $this->readFile();
+                $vehicle->counters()->delete();
 
-            foreach ($operations->groupBy('value') as $chunk) {
-                if ($chunk[0]['value'] < 1) {
-                    continue;
+                foreach($plans as $plan) {
+                    if ($plan->kms > 0) {
+                        $vehicle->counters()->save(new VehicleWorkCounter([
+                            'plan_id' => $plan->id,
+                            'vehicle_category' => $plan->vehicle_category,
+                            'max' => $plan->kms,
+                            'type' => 'kms',
+                            'description' => $plan->fullname,
+                        ]));
+                    }
+                    if ($plan->natural_hours > 0) {
+                        $vehicle->counters()->save(new VehicleWorkCounter([
+                            'plan_id' => $plan->id,
+                            'vehicle_category' => $plan->vehicle_category,
+                            'max' => $plan->natural_hours,
+                            'type' => 'natural_hours',
+                            'description' => $plan->fullname,
+                        ]));
+                    }
+                    if ($plan->work_hours > 0) {
+                        $vehicle->counters()->save(new VehicleWorkCounter([
+                            'plan_id' => $plan->id,
+                            'vehicle_category' => $plan->vehicle_category,
+                            'max' => $plan->work_hours,
+                            'type' => 'work_hours',
+                            'description' => $plan->fullname,
+                        ]));
+                    } elseif ($plan->can_hours > 0) {
+                        $vehicle->counters()->save(new VehicleWorkCounter([
+                            'plan_id' => $plan->id,
+                            'vehicle_category' => $plan->vehicle_category,
+                            'max' => $plan->can_hours,
+                            'type' => 'work_hours',
+                            'description' => $plan->fullname,
+                        ]));
+                    }
                 }
 
-                $name = "{$this->argument('prefix_plan_name')} {$chunk[0]['value']}".($this->argument('period_type') == 'kms' ? 'km' : 'h');
-                $plan = MaintenancePlan::create([
-                    'name' => $name,
-                    $this->argument('period_type') => $chunk[0]['value'],
-                ]);
-
-                foreach ($chunk as $item) {
-                    MaintenancePlanOperation::create([
-                        'maintenance_plan_id' => $plan->id,
-                        'family_id' => OperationFamily::firstOrCreate(['name' => $item['area']])->id,
-                        'name' => $item['operation'],
-                    ]);
-                }
-
-                $fleet->customPlans()->attach($plan);
-
-                foreach (collect($vehicles)->filter() as $vehicle) {
-                    $vehicle->counters()->save(new VehicleWorkCounter([
-                        'plan_id' => $plan->id,
-                        'type' => $this->argument('period_type'),
-                        'vehicle_category' => $this->argument('vehicle_category'),
-                        'max' => $plan->{$this->argument('period_type')},
-                        'description' => "{$fleet->name} - {$name}",
-                    ]));
-                }
-
-                $this->info("{$fleet->name} - {$name}");
+                Artisan::call("maintenance:sync {$vehicle->id}");
+                $this->info($vehicle->plate);
             }
 
             DB::commit();
@@ -88,26 +106,4 @@ class ImportaMaintenancePlanCommand extends Command
         }
     }
 
-    private function readFile()
-    {
-        $result = collect([]);
-        if (($handle = fopen($this->argument('file'), 'r')) !== false) {
-            $row = 0;
-            while (($data = fgetcsv($handle, 10000, ',')) !== false) {
-                if ($row == 0) {
-                    $row++;
-                    continue;
-                }
-
-                $result->push([
-                    'value' => (int) $data[1],
-                    'area' => $data[2],
-                    'operation' => $data[3],
-                ]);
-            }
-            fclose($handle);
-        }
-
-        return $result;
-    }
 }
--- app/Console/Commands/TestCommand.php
+++ app/Console/Commands/TestCommand.php
@@ -13,6 +13,8 @@ use App\User;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Hash;
 use Maatwebsite\Excel\Facades\Excel;
+use Illuminate\Support\Facades\Artisan;
+
 
 
 class TestCommand extends Command
@@ -38,20 +40,73 @@ class TestCommand extends Command
      */
     public function handle()
     {
-        /*$moba = new MobaClient(
-            config('services.moba.acciona_martorell.base_url'),
-            config('services.moba.acciona_martorell.username'),
-            config('services.moba.acciona_martorell.password')
-        );
+        $map = [
+            ["0287MLJ","(FABR) Citröen eBerlingo 100 kW"],
+            ["0413MDJ","(FABR) Iveco Daily GNC"],
+            ["0786LZD","(FABR) Invicta Metro"],
+            ["1009LZD","(FABR) Invicta Metro"],
+            ["1138LZD","(FABR) Invicta Metro"],
+            ["1212LZD","(FABR) Invicta Metro"],
+            ["1748MDY","(FABR) Iveco Daily GNC"],
+            ["1749MDY","(FABR) Iveco Daily GNC"],
+            ["1750MDY","(FABR) Iveco Daily GNC"],
+            ["1751MDY","(FABR) Iveco Daily GNC"],
+            ["1753MDY","(FABR) Iveco Daily GNC"],
+            ["1829JSP","(FABR) Peugeot Rifter K9 Diésel"],
+            ["1835JSP","(FABR) Peugeot Rifter K9 Diésel"],
+            ["2227JRN","(FABR) Peugeot Rifter K9 Diésel"],
+            ["2794JRJ","(FABR) Peugeot Rifter K9 Diésel"],
+            ["3282LYL","(FABR) Renault D15 Diésel"],
+            ["3450LZF","(FABR) Invicta Metro"],
+            ["3468LZF","(FABR) Invicta Metro"],
+            ["3485LZF","(FABR) Invicta Metro"],
+            ["4501MJV","(FABR) Iveco Eurocargo GNC"],
+            ["4508MJV","(FABR) Iveco Eurocargo GNC"],
+            ["4573LZD","(FABR) Renault Master Z.E."],
+            ["5127HDT","(FABR) Piaggio Porter 1.2 D120 E5"],
+            ["5314LZR","(FABR) Renault D15 Diésel"],
+            ["5357NCY","(FABR) Peugeot eExpert 100 kW"],
+            ["5679HLF","(FABR) Iveco Daily Diésel"],
+            ["5713MDW","(FABR) Iveco Daily GNC"],
+            ["5967HGT","(FABR) Peugeot Rifter K9 Diésel"],
+            ["5970MHS","(FABR) Iveco S-Way NP - AD260S"],
+            ["5971NCY","(FABR) Citröen Jumpy"],
+            ["5972HGT","(FABR) Iveco Daily Diésel"],
+            ["6027JRM","(FABR) Peugeot Rifter K9 Diésel"],
+            ["6286HMH","(FABR) Mercedes-Benz Axor"],
+            ["6287HMH","(FABR) Mercedes-Benz Axor"],
+            ["7124CDS","(FABR) Iveco Eurocargo GNC"],
+            ["7179CDS","(FABR) Iveco Eurocargo GNC"],
+            ["7794HGW","(FABR) Iveco Daily Diésel"],
+            ["7922LZB","(FABR) Renault Master Z.E."],
+            ["8452LZB","(FABR) Renault Master Z.E."],
+            ["8578HLP","(FABR) Piaggio Porter 1.3 Multitech E5"],
+            ["8653MKR","(FABR) Renault D26Wide CNG"],
+            ["8673HGT","(FABR) Peugeot Rifter K9 Diésel"],
+            ["8740CFD","(FABR) Iveco Eurocargo GNC"],
+            ["9349HHN","(FABR) Mercedes-Benz Axor"],
+            ["9556MHW","(FABR) Renault D26Wide CNG"],
+            ["C9707BWP","(FABR) Scoobic Light"],
+            ["E0736BHT","(FABR) Schmidt CleanGo 500 E6c"],
+            ["E0740BHT","(FABR) Schmidt CleanGo 500 E6c"],
+            ["E0742BHT","(FABR) Schmidt CleanGo 500 E6c"],
+            ["E0745BHT","(FABR) Schmidt CleanGo 500 E6c"],
+            ["E0746BHT","(FABR) Schmidt CleanGo 500 E6c"],
+            ["E2593BHS","(FABR) Tenax Electra 2.0 Evo"],
+            ["E3317BFZ","SCHMIDT SWINGO"],
+            ["E4337BHR","(FABR) Tenax Electra 2.0 Hydro"],
+            ["E4968BHS","(FABR) Tenax Electra 2.0 Hydro"],
+            ["E5063BHR","(FABR) Tenax Electra 2.0 Evo"],
+            ["E6698BHS","(FABR) Tenax Electra 2.0 Hydro"],
+            ["E7118BHR","(FABR) Schmidt eSwingo 200"],
+            ["E8470BHR","(FABR) Schmidt CleanGo 500 E6c"],
+            ["E8474BHR","(FABR) Schmidt CleanGo 500 E6c"],
+        ];
 
-        $a = $moba->getKms('8425MTC', '20/01/2005 00:00:00', '20/01/2025 23:59:59');*/
-
-        $client = new DistromelClient(
-            config('services.distromel.acciona.base_url'),
-            config('services.distromel.acciona.username'),
-            config('services.distromel.acciona.password'),
-            config('services.distromel.acciona_la_eliana.key'),
-        );
-        dd($client->getResourceStats(6));
+        foreach($map as $item) {
+            $vehicle = Vehicle::where('plate', $item[0])->first();
+            $this->info($vehicle->plate);
+            Artisan::call("maintenance-plan:import 30 '{$item[0]}' '{$item[1]}'");
+        }
     }
 }
