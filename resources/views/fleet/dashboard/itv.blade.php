3537c6ee93009aa7a87e4da8a3eff6c5ad5f467f
Luis Sanchez <lsanchez@zululabs.es>
2025-08-20 09:51:52 +0200

    change order itv

--- resources/views/fleet/dashboard/itv.blade.php
+++ resources/views/fleet/dashboard/itv.blade.php
@@ -74,8 +74,20 @@
 	@endcomponent
 
 	<br>
-	
 	<div>
+		<h3 class="text-lg leading-6 font-medium text-gray-900">
+			{{ __('Caducadas') }}
+		</h3>
+	</div>
+	<div class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
+		@foreach($expired as $vehicle) 	
+			@if(!$ongoing->contains($vehicle))
+				@include('fleet.dashboard.itv_card')
+			@endif
+		@endforeach
+	</div>
+
+	<div class="mt-8">
 		<h3 class="text-lg leading-6 font-medium text-gray-900">
 			{{ __('En curso') }}
 		</h3>
@@ -108,18 +120,5 @@
 				@endforeach
 			</div>
 		@endforeach
-
-	<div class="mt-8">
-		<h3 class="text-lg leading-6 font-medium text-gray-900">
-			{{ __('Caducadas') }}
-		</h3>
-	</div>
-	<div class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
-		@foreach($expired as $vehicle) 	
-			@if(!$ongoing->contains($vehicle))
-				@include('fleet.dashboard.itv_card')
-			@endif
-		@endforeach
-	</div>
 
 @endsection
