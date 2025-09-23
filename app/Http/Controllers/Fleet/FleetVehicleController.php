491b81afaeefb0ba199da471c00ccec67374b370
Daniel Ramirez <dani.rg15@gmail.com>
2025-09-23 11:47:30 +0200

    fix

--- app/Http/Controllers/Fleet/FleetVehicleController.php
+++ app/Http/Controllers/Fleet/FleetVehicleController.php
@@ -74,7 +74,7 @@ class FleetVehicleController extends Controller
             'expense_data' => $expense_data,
             'vehicle' => $vehicle,
             'states' => RepairOrderState::all(),
-            'repair_orders' => RepairOrder::where($filters)->latest()->paginate(50),
+            'repair_orders' => RepairOrder::where($filters)->where('vehicle_id', $vehicle->id)->latest()->paginate(10),
         ]);
     }
 
