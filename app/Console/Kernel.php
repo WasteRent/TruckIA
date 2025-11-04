33886257192312feffb14835b43bc368abb875d7
Daniel Ramirez <dani.rg15@gmail.com>
2025-11-04 15:19:43 +0100

    add movista acciona to scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -40,6 +40,8 @@ class Kernel extends ConsoleKernel
         $schedule->command('tracking:acciona-wemob')->everyFifteenMinutes();
         $schedule->command('tracking:acciona-moba')->hourly();
         $schedule->command('tracking:acciona-chip2chip')->hourly();
+        $schedule->command('tracking:acciona-movisat')->hourly();
+
         $schedule->command('tracking:svat-wemob')->everyFifteenMinutes();
         $schedule->command('tracking:svat-movisat')->everyFifteenMinutes();
 
