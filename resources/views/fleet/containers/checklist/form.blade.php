3d1c52c0d6daf7eb4a7333e863465bc12597a421
Saul <sromero@zululabs.es>
2025-12-03 14:09:42 +0100

    fix color

--- resources/views/fleet/containers/checklist/form.blade.php
+++ resources/views/fleet/containers/checklist/form.blade.php
@@ -5,7 +5,7 @@
             <fieldset class="mt-4 border-0 px-0">
                 <div class="relative flex items-start">
                     <div class="flex items-center h-5">
-                        {!! Form::checkbox("items[$item->id]", 1, $item->is_checked, ['class' => 'focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded']) !!}
+                        {!! Form::checkbox("items[$item->id]", 1, $item->is_checked, ['class' => 'focus:ring-green-500 h-4 w-4 text-green-600 border-gray-300 rounded']) !!}
                     </div>
                     <div class="ml-3 text-sm">
                         <label class="font-medium text-gray-700">{{$item->checklistItem->description}}</label>
