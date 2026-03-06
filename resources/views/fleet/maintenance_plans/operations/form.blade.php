0a9995d0c4b66c84f6d620c67ca875103ad526f9
Saul <sromero@zululabs.es>
2026-03-06 13:00:46 +0100

    añadido permiso para editar descripcion

--- resources/views/fleet/maintenance_plans/operations/form.blade.php
+++ resources/views/fleet/maintenance_plans/operations/form.blade.php
@@ -55,6 +55,6 @@
     <label class="form-label" >
       Descripción
     </label>
-    {!! Form::textarea('description', null, ['class' => 'appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500', 'readonly' => auth()->user()->job == 'contract_manager']) !!}
+    {!! Form::textarea('description', null, ['class' => 'appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500']) !!}
   </div>
 </div>
