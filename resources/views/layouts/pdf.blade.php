149f9b3e708b0570b47b6b91b979f06c2e6404f1
Daniel Ramirez <dani.rg15@gmail.com>
2026-03-25 18:31:10 +0100

    fix pdf

--- resources/views/layouts/pdf.blade.php
+++ resources/views/layouts/pdf.blade.php
@@ -5,7 +5,27 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" type="text/css" href="{{ url(mix('css/app.css')) }}">
+
+  <style type="text/css">
+
+	thead {
+  	    display: table-row-group;
+  	}
+	tr {
+  	    page-break-inside: avoid;
+  	}
+  	table {
+  	    word-wrap: break-word;
+  	}
+  	table td {
+  	    word-break: break-all;
+  	}
+
+
+  </style>
 </head>
+
+
 
 
 <body class="p-8">
